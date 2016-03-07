57c9b173f8a458b99606b77bf6f3570a45b7f50c
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-03-06 17:40:12 -0800

    Add preliminary starting code of form submission

--- src/containers/modal-container.js
+++ src/containers/modal-container.js
@@ -8,30 +8,36 @@ class ModalContainer extends Component {
   constructor(props) {
     super(props);
     this.close = this.close.bind(this);
+    this.save = this.save.bind(this);
   }
   close() {
     this.props.triggerModal();
   }
+  save(e) {
+    e.preventDefault();
+    console.log(this.refs.recipeName.getValue());
+    this.close();
+  }
   render() {
     return (
       <div className='row'>
-      <Button bsSize='large' bsStyle='primary' onClick={this.close}>
-        Add Recipe
-      </Button>
+        <Button bsSize='large' bsStyle='primary' onClick={this.close}>
+          Add Recipe
+        </Button>
         <Modal show={this.props.showModal} onHide={this.close}>
           <Modal.Header closeButton>
             <Modal.Title>Recipe</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <form>
-              <Input type="text" label="Recipe Name" placeholder="What are we making?" />
-              <Input type="textarea" label="Ingredients" placeholder="list, separated, by, comma" />
-              <Input type="textarea" label="Directions" placeholder="What are we going to do with these ingredients?" />
+            <form onSubmit={this.save}>
+              <Input type="text" label="Recipe Name" ref='recipeName' placeholder="What are we making?" />
+              <Input type="textarea" label="Ingredients" ref='ingredients' placeholder="list, separated, by, comma" />
+              <Input type="textarea" label="Directions" ref='directions' placeholder="What are we going to do with these ingredients?" />
             </form>
           </Modal.Body>
           <Modal.Footer>
             <Button onClick={this.close}>Cancel</Button>
-            <Button bsStyle='primary'>Save Recipe</Button>
+            <Button bsStyle='primary' type='submit' onClick={this.save}>Save Recipe</Button>
           </Modal.Footer>
         </Modal>
       </div>
