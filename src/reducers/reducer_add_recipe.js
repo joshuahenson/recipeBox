9a022cffa4272aa6c48bab0201beff7e89f053e2
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-03-22 20:42:27 -0700

    Clean up old code

--- src/reducers/reducer_add_recipe.js
+++ src/reducers/reducer_add_recipe.js
@@ -1,11 +1,8 @@
 // State argument is not application state
 // Only the state this reducer is responsible for
-
-//########################INCOMPLETE COPIED FROM SHOW_MODAL
 export default function(state = [], action) {
   switch(action.type) {
     case 'ADD_RECIPE':
-      //return Object.assign({}, state, action.payload);
       return [...state, action.payload];
   }
   return state;
