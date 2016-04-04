b8203595ac8a79681bde85c2dc8a87a8141c5498
Joshua Henson <praestigias@yahoo.com>
2016-04-04 22:37:08 +0000

    Remove old code

--- src/reducers/reducer_recipes.js
+++ src/reducers/reducer_recipes.js
@@ -1,14 +1,3 @@
-// State argument is not application state
-// Only the state this reducer is responsible for
-// export default function(state = [], action) {
-//   switch(action.type) {
-//     case 'ADD_RECIPE':
-//       return [...state, action.payload];
-//     default:
-//       return state
-//   }
-// }
-//////////////////////
 const recipe = (state, action) => {
   switch (action.type) {
     case 'ADD_RECIPE':
