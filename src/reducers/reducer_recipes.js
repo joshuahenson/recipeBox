666e9f3787a36ad02eed099ed505dd91518efca5
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-06-08 08:43:47 -0700

    Updated recipe example

--- src/reducers/reducer_recipes.js
+++ src/reducers/reducer_recipes.js
@@ -24,9 +24,12 @@ const recipe = (state, action) => {
 
 const exampleState = {
   id: 'x',
-  recipeName: 'Pizza',
-  ingredients: ['Phone', 'Phone number of pizza place'],
-  directions: 'Call pizza place'};
+  recipeName: 'Pancakes',
+  ingredients: ['1 1/2 cups all-purpose flour', '3 1/2 teaspoons baking powder', '1 teaspoon salt',
+    '1 tablespoon white sugar', '1 1/4 cups milk', '1 egg', '3 tablespoons melted butter'],
+  directions: 'In a large bowl, sift together the flour, baking powder, salt and sugar.' +
+  ' Make a well in the center and pour in the milk, egg and melted butter; mix until smooth.'
+};
 
 const recipes = (state = [exampleState], action) => {
   switch (action.type) {
